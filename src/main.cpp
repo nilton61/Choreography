e5b85b02b60b49441f11352c41c6ff852aff1ef1
nilton61 <nilton61@gmail.com>
2025-03-28 04:39:50 +0100

    Fyllt på choreography klassen

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,41 @@
 #include <Arduino.h>
 
-unsigned long previousMillis = 0;
-const long interval = 1000;
-bool ledState = LOW;
+typedef void stance();            //Tilståndsfunktion
+typedef stance* stancePointer;    //Pekre till tillståndsfunkrion
+
+class Choreography {
+  public:
+    stancePointer currentStance;  //Aktuellt tillstånd
+    unsigned long stanceStartTime;//Tidsstämpel
+    
+  public:
+    Choreography(stancePointer initialStance);
+    void dance();                 //Anropar det aktuella tillståndet
+    unsigned long timeInStance(); //Returnerar millisekunder i nuvarande tillstånd
+};//Choreography
+
+Choreography::Choreography(stancePointer initialStance) {//Constructor
+  currentStance = initialStance;  //Initialt tillstånd
+  stanceStartTime = millis();     //Tidsstämpel
+}//Constructor
+
+void Choreography::dance() {
+  currentStance();  //Anropa aktuellt tillstånd
+}//dance
+
+stance testFunction;
+void testFunction(){
+  Serial.println("Test function called");
+}//testFunction
+
+
+Choreography newbie(testFunction);
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("NodeMCU starting up...");
-  pinMode(LED_BUILTIN, OUTPUT);
-}
+  newbie.dance();
+}//setup()
 
 void loop() {
-  unsigned long currentMillis = millis();
-  
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    ledState = !ledState;
-    digitalWrite(LED_BUILTIN, ledState);
-    Serial.println(ledState ? "LED on" : "LED off");
-  }
-}
-  // Här kan andra uppgifter utföras utan att blockeras
+  // Resten av koden
+}//loop()
