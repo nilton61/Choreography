17a0124f956befe465a891e8daeca81e2f30e269
nilton61 <nilton61@gmail.com>
2025-03-28 08:29:08 +0100

    Implementerat quickstep() och passodoble()

--- src/main.cpp
+++ src/main.cpp
@@ -3,39 +3,67 @@
 typedef void stance();            //Tilståndsfunktion
 typedef stance* stancePointer;    //Pekre till tillståndsfunkrion
 
+
+
+
 class Choreography {
   private:
     stancePointer currentStance;  //Aktuellt tillstånd
-    unsigned long stanceStartTime;//Tidsstämpel
-    
+    unsigned long timeStamp;      //Tidsstämpel
+   
   public:
     Choreography(stancePointer initialStance);
     void dance();                 //Anropar det aktuella tillståndet
     unsigned long timeInStance(); //Returnerar millisekunder i nuvarande tillstånd
+    void quickstep(stancePointer nextStance); //Byter till nästa tillstånd
+    void passodoble(stancePointer setupFunc, stancePointer nextStance);
 };//Choreography
 
-Choreography::Choreography(stancePointer initialStance) {//Constructor
-  currentStance = initialStance;  //Initialt tillstånd
-  stanceStartTime = millis();     //Tidsstämpel
+
+// Implementera klassen
+Choreography::Choreography(stancePointer initialStance) {
+  currentStance = initialStance;
+  timeStamp = millis();
 }//Constructor
 
 void Choreography::dance() {
-  currentStance();  //Anropa aktuellt tillstånd
+  currentStance();
 }//dance
 
+void Choreography::quickstep(stancePointer nextStance) {
+  timeStamp = millis();
+  currentStance = nextStance;
+}//quickstep
+
+void Choreography::passodoble(stancePointer setupFunc, stancePointer nextStance) {
+  setupFunc();              // Kör setup-funktionen en gång
+  timeStamp = millis();     // Uppdatera tidsstämpeln
+  currentStance = nextStance; // Byt till nästa tillstånd
+}//passodoble
+
+// Fördeklarera funktioner
 stance testFunction;
-void testFunction(){
+stance idle;
+Choreography newbie(testFunction);//skapa global instans
+
+// Implementera funktionerna
+void  message() {
+  Serial.println("Switching to idle");
+}
+
+void testFunction() {
   Serial.println("Test function called");
+  newbie.passodoble(message, idle);
 }//testFunction
 
-
-Choreography newbie(testFunction);
+void idle() {
+  // Gör ingenting i detta tillstånd
+}//idle
 
 void setup() {
   Serial.begin(9600);
-  newbie.dance();
 }//setup()
 
 void loop() {
-  // Resten av koden
+  newbie.dance();
 }//loop()
