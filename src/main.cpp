8bddd2f66fc3f0a4af41160e6b5b5f96029a44ba
nilton61 <nilton61@gmail.com>
2025-04-05 08:50:48 +0200

    finalised time controlled hierachical state machine

--- src/main.cpp
+++ src/main.cpp
@@ -3,40 +3,21 @@
 
   enum {RED, YELLOW, GREEN, NCOLOR};
   enum {REDPIN = 14, YELLOWPIN, GREENPIN, SWITCH};
+  
+  // Main machine
+  stance forth(); stance back(); stance backAndForth();
+  Choreography manager(forth);
 
-  enum {FORTH, BACK, B_N_F, NPATTERN};
+  //Sub machine1, forward
+  stance forth1(); stance forth2(); stance forth3();
+  Choreography forthManager(forth1);
 
-  typedef byte LedPattern[NCOLOR];
-  const LedPattern lPattern[NCOLOR] = {
-    {1, 0, 0},  // RED
-    {0, 1, 0},  // YELLOW
-    {0, 0, 1}   // GREEN
-  };
+  // Sub machine2, backward
+  stance back1(); stance back2(); stance back3();
+  Choreography backManager(back1);
 
-  const LedPattern pinPattern = {REDPIN, YELLOWPIN, GREENPIN};
-
- 
-  stance forth();
-  stance back();
-  stance backAndForth();
-  Choreography manager(forth, micros);
-
-  stance forth1();
-  stance forth2();
-  stance forth3();
-  Choreography forthManager(forth1, micros);
-
-  stance back1();
-  stance back2();
-  stance back3();
-  Choreography backManager(back1, micros);
-
-  stance bNf1();
-  stance bNf2();
-  stance bNf3();
-  stance bNf4();
-  stance bNf5();
-  Choreography bNfManager(bNf1, micros);
+  stance bNf1(); stance bNf2(); stance bNf3(); stance bNf4();
+  Choreography bNfManager(bNf1);
 
   stance showRED(){
     digitalWrite(REDPIN, 1);
@@ -61,31 +42,35 @@
     pinMode(YELLOWPIN, OUTPUT);
     pinMode(GREENPIN, OUTPUT);
     pinMode(SWITCH, INPUT);
+    Serial.begin(9600);
   }//setup
 
   void loop() {
-    forthManager.dance();
+    manager.dance();
   }//loop
 
-  stance forth(){};
-  stance back(){};
-  stance backAndForth(){};
-  stance forth1(){
-    forthManager.sequence(500000, showRED, forth2);
+  stance forth(){
+    forthManager.dance();
+    manager.sequence(4500, back);
   };
 
-  stance forth2(){
-    forthManager.sequence(500000, showYELLOW, forth3);
+  stance back(){
+    backManager.dance();
+    manager.sequence(4500, backAndForth);
   };
 
-  stance forth3(){
-    forthManager.sequence(500000, showGREEN, forth1);
+  stance backAndForth(){
+    bNfManager.dance();
+    manager.sequence(4800, forth);
   };
-  stance back1(){};
-  stance back2(){};
-  stance back3(){};
-  stance bNf1(){};
-  stance bNf2(){};
-  stance bNf3(){};
-  stance bNf4(){};
-  stance bNf5(){};
+
+  stance forth1(){forthManager.sequence(500, showRED, forth2);};
+  stance forth2(){forthManager.sequence(500, showYELLOW, forth3);};
+  stance forth3(){forthManager.sequence(500, showGREEN, forth1);};
+  stance back1(){backManager.sequence(500, showGREEN, back2);};
+  stance back2(){backManager.sequence(500, showYELLOW, back3);};
+  stance back3(){backManager.sequence(500, showRED, back1);};
+  stance bNf1(){bNfManager.sequence(500, showRED, bNf2);};
+  stance bNf2(){bNfManager.sequence(500, showYELLOW, bNf3);};
+  stance bNf3(){bNfManager.sequence(500, showGREEN, bNf4);};
+  stance bNf4(){bNfManager.sequence(500, showYELLOW, bNf1);};
