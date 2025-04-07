4d66a8f0732fbf052e8028cca00c46dffc4ffdb7
nilton61 <nilton61@gmail.com>
2025-04-07 19:57:03 +0200

    ändrat kommentarer till engelska

--- src/Choreography.cpp
+++ src/Choreography.cpp
@@ -1,45 +1,45 @@
 #include <Arduino.h>
 #include <Choreography.h>
 
-// Implementera klassen
+// Implement Choreography class
   Choreography::Choreography(stancePointer initialStance, unsigned long (*timeFn)()) {
-    currentStance = initialStance;
-    timeFunc = timeFn;
-    timeStamp = timeFunc();
+    currentStance = initialStance;            //initial state
+    timeFunc = timeFn;                        //millis()(default) or micros
+    timeStamp = timeFunc();                   //create timestamp
   }//constructor
   
   void Choreography::sequence(unsigned long interval, stancePointer nextStance) {
     if (timeFunc() - timeStamp >= interval) { //timecheck
-      timeStamp = timeFunc();                 //ny timestamp
-      currentStance = nextStance;           //byt tillstånd nästa varv
+      timeStamp = timeFunc();                 //new timestamp
+      currentStance = nextStance;             //change state next scan cycle
     }//timecheck
   }//sequence med nextStance
   
-  // Kör setupFunc och byter sedan till nextStance efter angivet tidsintervall
+  // Call setupFunction and the change state next scancycle
   void Choreography::sequence(unsigned long interval, stancePointer setupFunc, stancePointer nextStance) {
-    if (timeFunc() - timeStamp >= interval) {//timecheck
-      setupFunc();                        // Kör setup-funktionen en gång
-      timeStamp = timeFunc();               // ny timestamp
-      currentStance = nextStance;         // byt tillstånd nästa varv
+    if (timeFunc() - timeStamp >= interval) { //timecheck
+      setupFunc();                            //run setupFunction only once
+      timeStamp = timeFunc();                 //new timestamp
+      currentStance = nextStance;             //change state next scan cycle
     }//timecheck
-  }//sequence med setupFunc och nextStance
+  }//sequence with setupFunc and nextStance
   
   void Choreography::dance() {
     currentStance();
   }//dance
   
   unsigned long Choreography::timeInStance() {
-    return timeFunc() - timeStamp;
+    return timeFunc() - timeStamp;            //calculate time overflow safe
   }//timeInStance
   
   void Choreography::quickstep(stancePointer nextStance) {
-    timeStamp = timeFunc();
+    timeStamp = timeFunc();                    //calculate time overflow safe
     currentStance = nextStance;
   }//quickstep
   
   void Choreography::passodoble(stancePointer setupFunc, stancePointer nextStance) {
-    setupFunc();              // Kör setup-funktionen en gång
-    timeStamp = timeFunc();     // Uppdatera tidsstämpeln
-    currentStance = nextStance; // Byt till nästa tillstånd
+    setupFunc();                              //run setupFunction only once
+    timeStamp = timeFunc();                   //update timeStamp 
+    currentStance = nextStance;               //change state next scan cycle
   }//passodoble
   
